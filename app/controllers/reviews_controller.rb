f80f8c57f005a4f91bd19248c61ac0a11a70805a
samirv16 <samir.vallecillo@outlook.com>
2019-12-05 18:14:56 -0600

    adds user controller and reviews index view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
 
+    def index
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            @reviews = @user.reviews 
+        else
+            @reviews = Review.all 
+        end
+    end
     
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :car_rentals
+  resources :car_rentals do
+    resources :reviews
+  end
   resources :owners
   devise_for :users, :controllers => {registrations: 'registrations' }
   root to: 'application#home'

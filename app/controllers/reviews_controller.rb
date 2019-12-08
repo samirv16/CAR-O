3d095db380575993e8c4940702ae94312647f515
samirv16 <samir.vallecillo@outlook.com>
2019-12-08 17:56:57 -0600

    modifies create and new method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,35 @@
 class ReviewsController < ApplicationController
 
     def index
-        if params[:user_id]
-            @user = User.find(params[:user_id])
+        if @user = current_user
             @reviews = @user.reviews 
+            
         else
             @reviews = Review.all 
         end
     end
+
+    
+    def new
+        @review =Review.new
+    end
+
+    def create
+        @review = Review.new(review_params) #current_user.reviews.build
+        if @review.save
+            redirect_to car_rental_path #review_path(@review)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:comment, :rating)
+    end
+
+    
     
 
 
